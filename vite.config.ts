25c03c2aadcc3a2bc0ea63723645e7cd66694b3c
HasithaGallella <hbgallella@gmail.com>
2025-12-07 21:57:40 +0530

    Basic code structure for the Vite app to demo the  new LLaVA-MedRAG

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/LLaVA-MedRAG.github.io/',
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 5173,      // Your port
+    strictPort: true // Fail if port is already in use
+  }
+})
